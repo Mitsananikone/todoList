1abe07f8ce52330e722e35e73dd21fc9d32c318e
Mit Sananikone <Mitsananikone@gmail.com>
2022-09-22 16:21:27 -0500

    Added ability to delete list items

--- index.js
+++ index.js
@@ -1,53 +1,42 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import "./index.css";
-
-function App() {
-
-
+function App(){
   const [todos, setTodos] = React.useState([
     {
-        text: 'learn react',
-        isCompleted: false,
+      text: 'learn react',
+      isCompleted: false,
     },
     {
-        text: 'meet friend for lunch',
-        isCompleted: false,
+      text: 'meet friend for lunch',
+      isCompleted: false,
     },
     {
-        text: 'build todo app',
-        isCompleted: false,
-    }
+      text: 'build todo app',
+      isCompleted: false,
+    }        
   ]);
 
-  const [value, setValue] = React.useState('');
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    const newTodos = [...todos, {text:value, isCompleted:false}];
+  const addTodo = text => {
+    const newTodos = [...todos, {text, isCompleted:false}];
     setTodos(newTodos);
-    setValue('');
+  }
+  const removeTodo = index => {
+    let temp = [...todos];    
+    temp.splice(index, 1);
+    setTodos(temp);
   }
 
-  return (<>
-    {todos.map((todo,i) => 
-    <div className="todo" key={i}>{todo.text}</div>)}
-
-    <form onSubmit={handleSubmit}>
-      <input type="text" className="input"
-      value={value}
-      placeholder="Add ToDo..."
-      onChange={e=> setValue(e.target.value)}
-      />
-    </form>
-
-
-    </>);
+  return(
+    <div className="app">
+      <div className="todo-list" >
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
 }
 
 ReactDOM.render(
   <App/>,
   document.getElementById('root')
-)
-
+);
